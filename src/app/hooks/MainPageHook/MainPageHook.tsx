a0be6975ac4f11d8bb2ffcc234dd3296395aab51
lautaro3870 <lautarodisbro@gmail.com>
2024-11-03 11:26:19 +0100

    feat: re added filter to localStorage

--- src/app/hooks/MainPageHook/MainPageHook.tsx
+++ src/app/hooks/MainPageHook/MainPageHook.tsx
@@ -18,8 +18,7 @@ export default function MainPageHook() {
   const [listCards, setListCards] = useState<ListCardstype>([]);
 
   useEffect(() => {
-    // const filters = JSON.parse(localStorage.getItem("filters") || "");
-    const filters = {area: [], departamento: "", personal: [], anio: []};
+    const filters = JSON.parse(localStorage.getItem("filters") || "{}");
     const fetchProjects = async () => {
       setDataLoaded(false);
       const projects = getProjectsFiltered(
