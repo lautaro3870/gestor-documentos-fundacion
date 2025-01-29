5be02e4d08fa09f690fa1f262088aa39a16e310a
lautaro3870 <lautarodisbro@gmail.com>
2025-01-29 19:40:16 +0100

    feat: scroll to the middle when is mobile screen

--- src/app/components/Pagination/Paginator.tsx
+++ src/app/components/Pagination/Paginator.tsx
@@ -3,13 +3,19 @@ import * as React from "react";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import { useAppContext } from "@/context";
+import { useMediaQuery } from "@mui/material";
 
 export default function Paginator() {
   const { paginator, setPage, page } = useAppContext();
+  const isMobile = useMediaQuery("(max-width:600px)");
 
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    window.scrollTo({
+      top: !isMobile ? 0 : document.body.scrollHeight / 3.5,
+      left: 0,
+      behavior: "smooth",
+    });
   };
 
   return (
