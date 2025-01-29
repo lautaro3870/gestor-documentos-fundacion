581808763783552a0ba7522bac38e632dc3bd765
lautaro3870 <lautarodisbro@gmail.com>
2025-01-29 19:09:44 +0100

    feat: display message when the are no documents to display

--- src/app/components/ListProjects/ListProjects.tsx
+++ src/app/components/ListProjects/ListProjects.tsx
@@ -48,96 +48,117 @@ export default function ListProjects({ projects }: ListProjectsProps) {
   return (
     <div className={isLoading ? styles.isLoading : ""}>
       {isLoading ? (
-        <CircularProgress sx={{marginTop: "6rem" }} size="3rem" />
+        <CircularProgress sx={{ marginTop: "6rem" }} size="3rem" />
       ) : (
         <div>
-          {currentProjects.map((project: ListCardsStruct, index: number) => (
-            <Card
-              variant="outlined"
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-start",
-                height: {
-                  lg: "16rem",
-                  xs: "20rem",
-                },
-                width: {
-                  lg: "75rem",
-                  md: "55rem",
-                  xs: "80%",
-                  sm: "50rem",
-                },
-                margin: "auto",
-              }}
-              key={index}
-            >
-              <CardMedia
-                component="img"
+          {currentProjects.length === 0 ? (
+            <div>
+              <Typography
+                variant="h5"
+                marginBottom="10rem"
+                marginTop="6rem"
                 sx={{
-                  height: 100,
-                  width: 200,
-                  objectFit: "contain",
-                  display: {
-                    xs: "none",
-                    xl: "block",
-                    lg: "block",
-                  },
-                  marginLeft: "1rem",
-                }}
-                image="https://fundacionbariloche.org.ar/wp-content/uploads/2024/07/pdf.png"
-              />
-              <CardContent
-                sx={{
-                  marginRight: "auto",
-                  width: {
-                    lg: "50rem",
-                    xs: "20rem",
-                  },
+                  marginTop: "3rem",
+                  textAlign: "center",
+                  color: "text.secondary",
                 }}
               >
-                <Typography
-                  gutterBottom
-                  sx={{ fontSize: "1rem" }}
-                  component="div"
-                >
-                  {project.departamento} - {project.anio_inicio}
-                </Typography>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                  fontSize={{
-                    xs: "0.9rem",
-                  }}
-                >
-                  {project.titulo}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  sx={{
-                    color: "text.secondary",
-                    overflow: {
-                      xs: "auto",
-                    },
-                  }}
-                >
-                  {getPersonalList(project)}
-                </Typography>
-              </CardContent>
-              <div
-                style={{
-                  marginRight: "1rem",
-                }}
-              >
-                <Link href={`/project/${project.id}`}>
-                  <Button>
-                    <AddIcon />
-                  </Button>
-                </Link>
-              </div>
-            </Card>
-          ))}
+                No hay proyectos que mostrar
+              </Typography>
+            </div>
+          ) : (
+            <div>
+              {currentProjects.map(
+                (project: ListCardsStruct, index: number) => (
+                  <Card
+                    variant="outlined"
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "flex-start",
+                      height: {
+                        lg: "16rem",
+                        xs: "20rem",
+                      },
+                      width: {
+                        lg: "75rem",
+                        md: "55rem",
+                        xs: "80%",
+                        sm: "50rem",
+                      },
+                      margin: "auto",
+                    }}
+                    key={index}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        height: 100,
+                        width: 200,
+                        objectFit: "contain",
+                        display: {
+                          xs: "none",
+                          xl: "block",
+                          lg: "block",
+                        },
+                        marginLeft: "1rem",
+                      }}
+                      image="https://fundacionbariloche.org.ar/wp-content/uploads/2024/07/pdf.png"
+                    />
+                    <CardContent
+                      sx={{
+                        marginRight: "auto",
+                        width: {
+                          lg: "50rem",
+                          xs: "20rem",
+                        },
+                      }}
+                    >
+                      <Typography
+                        gutterBottom
+                        sx={{ fontSize: "1rem" }}
+                        component="div"
+                      >
+                        {project.departamento} - {project.anio_inicio}
+                      </Typography>
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                        fontSize={{
+                          xs: "0.9rem",
+                        }}
+                      >
+                        {project.titulo}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          color: "text.secondary",
+                          overflow: {
+                            xs: "auto",
+                          },
+                        }}
+                      >
+                        {getPersonalList(project)}
+                      </Typography>
+                    </CardContent>
+                    <div
+                      style={{
+                        marginRight: "1rem",
+                      }}
+                    >
+                      <Link href={`/project/${project.id}`}>
+                        <Button>
+                          <AddIcon />
+                        </Button>
+                      </Link>
+                    </div>
+                  </Card>
+                )
+              )}
+            </div>
+          )}
         </div>
       )}
     </div>
